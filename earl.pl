cb616aa405e23df49d34793d0c1ee740236c99c3
Jonty <jonty@jonty.co.uk>
2010-10-08 15:43:57 +0100

    Support new-style twitter URL's

--- earl.pl
+++ earl.pl
@@ -61,7 +61,7 @@ sub get_response {
     return "$headline \x{2014} $summary";
   }
   # Twitter status: screen name and tweet
-  elsif ( $url =~ m'^http://twitter.com/\w+/status(?:es)?/\d+$' ) {
+  elsif ( $url =~ m'^http://twitter.com/(#!/)?\w+/status(?:es)?/\d+$' ) {
     $head->parse( get( $url ) );
     my $name = $head->header( 'X-Meta-Page-user-screen_name' );
     my $tweet = $head->header( 'X-Meta-Description');
